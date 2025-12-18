209fbda95c8542525e0000ad24a4de697bfa3e0b
Cafesuada <108068915+cafesuada24@users.noreply.github.com>
2025-12-19 00:56:15 +0700

    simulator-service: feat: publish robot state on every tick

--- services/simulator/app/world/simulator.py
+++ services/simulator/app/world/simulator.py
@@ -4,23 +4,28 @@ from enum import Enum
 from queue import Queue
 from time import monotonic
 
+from app.infra.bus.event_bus import EventBus
+from app.infra.mappers.robot_state_mapper import snapshot_to_proto
+
 from .models.world import World
 
 
-class Simulator:
-    class Command(Enum):
-        WAIT = 1
-        MOVE_TO = 2
-        PICKUP = 3
-        DROPOFF = 4
+class SimulatorCommand(Enum):
+    WAIT = 1
+    MOVE_TO = 2
+    PICKUP = 3
+    DROPOFF = 4
 
-    def __init__(self, world: World) -> None:
+class Simulator[T]:
+
+    def __init__(self, world: World, bus: EventBus[T]) -> None:
         self.__world = world
         self.__tick_hz = 10
         self.__running = False
-        self.__command_queue = Queue[tuple[Simulator.Command, str]]()
+        self.__command_queue = Queue[tuple[SimulatorCommand, str]]()
+        self.__bus = bus
 
-    def register_command(self, command: Command, data: str) -> None:
+    def register_command(self, command: SimulatorCommand, data: str) -> None:
         """Register a command to be executed."""
         self.__command_queue.put_nowait((command, data))
 
@@ -33,7 +38,9 @@ class Simulator:
             while self.__running:
                 t0 = monotonic()
                 await self.__tick(dt=max(0.0, dt - 0.001))
-                await asyncio.sleep(max(0, dt - (monotonic() - t0)))
+                elapsed = monotonic() - t0
+                await asyncio.sleep(max(0, dt - elapsed))
+                self.__world.time += max(dt, elapsed)
 
         except Exception as e:
             print(f'Simulator crashed: {e!r}')
@@ -44,12 +51,14 @@ class Simulator:
 
     async def __tick(self, dt: float) -> None:
         """Move the simulator forward an amount of 'dt' time."""
-        t_end = monotonic() + dt
-        while monotonic() < t_end:
+        # t_end = monotonic() + dt
+        while True:
             try:
                 cmd = self.__command_queue.get_nowait()
             except queue.Empty:
                 break
 
-            print(f'Executing {cmd}')
+        for robot in self.__world.robots.values():
+            proto_msg = snapshot_to_proto(robot.snapshot()).SerializeToString()
+            self.__bus.publish_event("ROBOT_STATE", proto_msg)
 
