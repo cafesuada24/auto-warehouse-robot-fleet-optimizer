8a40b2bd1d32d4ba6793da290bd8d04cade58503
Cafesuada <108068915+cafesuada24@users.noreply.github.com>
2025-12-19 23:15:13 +0700

    simulator-service: add Coordinate type

--- services/simulator/app/world/models/map.py
+++ services/simulator/app/world/models/map.py
@@ -2,11 +2,11 @@
 from pydantic import PositiveInt
 from pydantic.dataclasses import dataclass
 
-from app.world.types import Position
+from app.world.types import Coordinate
 
 
 @dataclass
 class Map:
     width: PositiveInt
     height: PositiveInt
-    obstacles: set[Position]
+    obstacles: set[Coordinate]
