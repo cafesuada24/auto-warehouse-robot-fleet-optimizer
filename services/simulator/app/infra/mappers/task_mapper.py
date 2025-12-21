4479b4caf7f67079e5f3406bbf2f475de687ed9d
Cafesuada <108068915+cafesuada24@users.noreply.github.com>
2025-12-21 16:01:14 +0700

    simulator-service: required time sec changed to milisec

--- services/simulator/app/infra/mappers/task_mapper.py
+++ services/simulator/app/infra/mappers/task_mapper.py
@@ -5,19 +5,19 @@ from awrfo.contracts.envelopes.event_envelope.v1.event_envelope_pb2 import Event
 from awrfo.contracts.events.task.v1.task_completed_pb2 import TaskCompletedEvent
 from awrfo.contracts.events.task.v1.task_created_pb2 import TaskCreatedEvent
 from awrfo.contracts.schemas.common.v1.coordinate_pb2 import Coordinate
-from pydantic import NonNegativeFloat
+from pydantic import NonNegativeInt
 
 from app.world.models.task import TaskSnapshot
 
 
 def taskcreated_snapshot_to_proto(
     snapshot: TaskSnapshot,
-    ts_s: NonNegativeFloat,
+    ts_ms: NonNegativeInt,
 ) -> TaskCreatedEvent:
     envelope = EventEnvelope(
         event_id=str(uuid4()),
         event_type='task.created.v1',
-        ts_ms=int(ts_s * 1000),
+        ts_ms=ts_ms,
         producer='simulator-service',
         envelope_version=1,
     )
@@ -40,12 +40,12 @@ def taskcreated_snapshot_to_proto(
 def taskcompleted_snapshot_to_proto(
     snapshot: TaskSnapshot,
     robot_id: str,
-    ts_s: NonNegativeFloat,
+    ts_ms: NonNegativeInt,
 ) -> TaskCompletedEvent:
     envelope = EventEnvelope(
         event_id=str(uuid4()),
         event_type='task.completed.v1',
-        ts_ms=int(ts_s * 1000),
+        ts_ms=ts_ms,
         producer='simulator-service',
         envelope_version=1,
     )
