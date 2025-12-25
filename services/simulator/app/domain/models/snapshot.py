9d48142ff947866d779eadc3beacbabe12b616e8
Cafesuada <108068915+cafesuada24@users.noreply.github.com>
2025-12-25 21:50:25 +0700

    simulator-service: update models

--- services/simulator/app/domain/models/snapshot.py
+++ services/simulator/app/domain/models/snapshot.py
@@ -2,7 +2,10 @@ from dataclasses import dataclass
 
 from pydantic.types import NonNegativeInt
 
+from app.types import IDType
+
 
 @dataclass(frozen=True)
 class Snapshot:
+    id: IDType
     timestamp_ms: NonNegativeInt
