0e17102bcfba6354be5a6684470f73a2857898e8
Cafesuada <108068915+cafesuada24@users.noreply.github.com>
2025-12-25 21:52:13 +0700

    simulator-service: implement threaded publisher

--- services/simulator/app/infra/bus/redis_bus.py
+++ services/simulator/app/infra/bus/redis_bus.py
@@ -5,8 +5,6 @@ import redis
 from awrfo.logging.logger import get_logger
 from redis.client import PubSub, PubSubWorkerThread
 
-from app.domain.models.robot import RobotStateSnapshot
-from app.domain.models.task import TaskSnapshot
 from app.infra.mappers import robot_state_mapper, task_mapper
 
 logger = get_logger('Redis')
@@ -73,28 +71,28 @@ class RedisBusAdapter:
     #         commands.append(cmd)
 
 
-class RedisEventPublisher:
-    def __init__(self, bus: RedisBusAdapter) -> None:
-        self.__bus = bus
-
-    def publish_task_completed_event(self, snapshot: TaskSnapshot) -> None:
-        proto_msg = task_mapper.taskcompleted_snapshot_to_proto(
-            snapshot,
-            '',
-            snapshot.timestamp_ms,
-        )
-        self.__bus.publish_event('TASK:COMPLETED', proto_msg.SerializeToString())
-
-    def publish_task_created_event(self, snapshot: TaskSnapshot) -> None:
-        event_to_publish = task_mapper.taskcreated_snapshot_to_proto(
-            snapshot,
-            snapshot.timestamp_ms,
-        )
-        self.__bus.publish_event('TASK:CREATED', event_to_publish.SerializeToString())
-
-    def publish_robot_state(self, snapshot: RobotStateSnapshot) -> None:
-        proto_msg = robot_state_mapper.snapshot_to_proto(
-            snapshot,
-            snapshot.timestamp_ms,
-        ).SerializeToString()
-        self.__bus.publish_event('ROBOT_STATE', proto_msg)
+# class RedisEventPublisher:
+#     def __init__(self, bus: RedisBusAdapter) -> None:
+#         self.__bus = bus
+#
+#     def publish_task_completed_event(self, snapshot: TaskSnapshot) -> None:
+#         proto_msg = task_mapper.taskcompleted_snapshot_to_proto(
+#             snapshot,
+#             '',
+#             snapshot.timestamp_ms,
+#         )
+#         self.__bus.publish_event('TASK:COMPLETED', proto_msg.SerializeToString())
+#
+#     def publish_task_created_event(self, snapshot: TaskSnapshot) -> None:
+#         event_to_publish = task_mapper.taskcreated_snapshot_to_proto(
+#             snapshot,
+#             snapshot.timestamp_ms,
+#         )
+#         self.__bus.publish_event('TASK:CREATED', event_to_publish.SerializeToString())
+#
+#     def publish_robot_state(self, snapshot: RobotStateSnapshot) -> None:
+#         proto_msg = robot_state_mapper.snapshot_to_proto(
+#             snapshot,
+#             snapshot.timestamp_ms,
+#         ).SerializeToString()
+#         self.__bus.publish_event('ROBOT_STATE', proto_msg)
