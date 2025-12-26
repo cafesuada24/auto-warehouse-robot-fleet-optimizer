72c5d93a222614b55ccdc0f3a9eddeb760bc88aa
Cafesuada <108068915+cafesuada24@users.noreply.github.com>
2025-12-26 16:31:26 +0700

    shared-common: add remove method and adjust testcases

--- shared/library/awrfo-python/src/awrfo/ttl_cache.py
+++ shared/library/awrfo-python/src/awrfo/ttl_cache.py
@@ -22,19 +22,19 @@ class TTLCache[V]:
         self.__max_size = max_size
         self.__expire_period_ms = expire_period_ms
         self.__set: set[V] = set()
-        self.__q: deque[_CacheEntry[V]] = deque(maxlen=max_size)
+        self.__q: deque[_CacheEntry[V]] = deque()
         self.__lock = threading.Lock()
 
     def try_add(self, value: V, ts_ms: int | None = None) -> bool:
         if ts_ms is None:
             if not self.__use_wall_timer:
-                raise ValueError('timestamp must be provided if use_wall_timer is set')
+                raise ValueError('timestamp must be provided unless use_wall_timer is set')
             ts_ms = datetime.now().microsecond
 
         with self.__lock:
-            self.__evict(ts_ms, ensure_avail=True)
+            self.__evict(ts_ms)
 
-            if value in self.__set:
+            if value in self.__set or len(self.__q) == self.__max_size:
                 return False
 
             expr = ts_ms + self.__expire_period_ms
@@ -42,9 +42,19 @@ class TTLCache[V]:
             self.__q.append(_CacheEntry(expire_ts_ms=expr, value=value))
             return True
 
-    def __evict(self, ts_ms: int, *, ensure_avail: bool = False) -> None:
-        while (self.__q and self.__q[0].expire_ts_ms <= ts_ms) or (
-            ensure_avail and len(self.__q) == self.__q.maxlen
-        ):
+    def remove(self, value: V, ts_ms: int | None = None) -> None:
+        if ts_ms is None:
+            if not self.__use_wall_timer:
+                raise ValueError('timestamp must be provided unless use_wall_timer is set')
+            ts_ms = datetime.now().microsecond
+
+        with self.__lock:
+            if value not in self.__set:
+                return
+            self.__set.discard(value)
+            self.__evict(ts_ms)
+
+    def __evict(self, ts_ms: int) -> None:
+        while self.__q and self.__q[0].expire_ts_ms <= ts_ms:
             val = self.__q.popleft()
             self.__set.discard(val.value)
