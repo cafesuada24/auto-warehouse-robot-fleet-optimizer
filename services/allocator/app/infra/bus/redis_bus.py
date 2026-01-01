e040e1eb1a7fef6d85b462a8e80491f46d9ef88d
Cafesuada <108068915+cafesuada24@users.noreply.github.com>
2026-01-01 21:16:07 +0700

    allocator-service: implement redis event bus

---
+++ services/allocator/app/infra/bus/redis_bus.py
@@ -0,0 +1,45 @@
+import redis
+from awrfo.logging.logger import get_logger
+from pydantic import NonNegativeFloat
+
+from services.allocator.app.application.ports.event_bus import EventMessage
+
+_logger = get_logger('redis_bus')
+
+
+class RedisBus:
+    def __init__(self, host: str = 'localhost', port: int = 6379) -> None:
+        self.__host = host
+        self.__port = port
+        self.__redis = redis.Redis(
+            host=self.__host,
+            port=self.__port,
+            db=0,
+            protocol=3,
+        )
+        self.__p = self.__redis.pubsub(ignore_subscribe_messages=True)  # pyright: ignore
+
+    def poll(self, timeout_s: NonNegativeFloat = 0.2) -> EventMessage | None:
+        msg = self.__p.get_message(timeout=timeout_s)
+        if msg is None:
+            return None
+        if (
+            'topic' not in msg
+            or 'data' not in msg
+            or not isinstance(msg['topic'], str)
+            or not isinstance(msg['data'], bytes)
+        ):
+            _logger.debug(f'Ignoring invalid message: {msg}')
+            return None
+
+        return EventMessage(
+            topic=msg['topic'],
+            payload=msg['data'],
+        )
+
+    def publish(self, topic: str, payload: bytes) -> None:
+        self.__redis.publish(channel=topic, message=payload)
+
+    def close(self) -> None:
+        self.__p.close()
+        self.__redis.close()
